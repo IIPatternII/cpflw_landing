69c6889395f93574af492e7295bff5127f2953bb
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-09-08 20:59:42 -0300

    - Added ContentBlockWithEmailCapture basic ConvertKitForm integration - Cleaned up index from unused components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,20 @@
 import { lazy } from "react";
 import IntroContent from "../../content/IntroContent.json";
-import MiddleBlockContent from "../../content/MiddleBlockContent.json";
 import AboutContent from "../../content/AboutContent.json";
 import MissionContent from "../../content/MissionContent.json";
 import ProductContent from "../../content/ProductContent.json";
-import ContactContent from "../../content/ContactContent.json";
 
-const Contact = lazy(() => import("../../components/ContactForm"));
-const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
 const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 const ContentBlockWithButtons = lazy(() => import("../../components/ContentBlock/ContentBlockWithButtons"));
 const ContentBlockWithSections = lazy(() => import("../../components/ContentBlock/ContentBlockWithSections"));
+const ContentBlockWithEmailCapture = lazy(() => import("../../components/ContentBlock/ContentBlockWithEmailCapture"));
 
 const Home = () => {
   return (
     <Container>
       <ScrollToTop />
-      <ContentBlockWithButtons
+      <ContentBlockWithEmailCapture
         fadeDirection="up"
         title={IntroContent.title}
         content={IntroContent.text}
@@ -25,11 +22,6 @@ const Home = () => {
         icon="developer.svg"
         id="intro"
       />
-      {/* <MiddleBlock
-        title={MiddleBlockContent.title}
-        content={MiddleBlockContent.text}
-        button={MiddleBlockContent.button}
-      /> */}
       <ContentBlockWithSections
         fadeDirection="left"
         title={AboutContent.title}
@@ -51,11 +43,6 @@ const Home = () => {
         content={ProductContent.text}
         icon="waving.svg"
         id="product"
-      />
-      <Contact
-        title={ContactContent.title}
-        content={ContactContent.text}
-        id="contact"
       />
     </Container>
   );
