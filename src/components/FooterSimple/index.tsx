0fe957a998ff442f9a9074be7ed5480984698e5f
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-09-11 16:37:41 -0300

    Added EmailCaputreForm to the SimpleFooter

--- src/components/FooterSimple/index.tsx
+++ src/components/FooterSimple/index.tsx
@@ -1,6 +1,8 @@
 import { Row, Col } from "antd";
 import { withTranslation } from "react-i18next";
+import CaptureEmailForm from "../../common/CaptureEmailForm";
 import Container from "../../common/Container";
+import HeroContent from "../../content/HeroContent.json";
 
 import {
   FooterSection,
@@ -19,35 +21,22 @@ const FooterSimple = ({ t }: any) => {
       <FooterSection>
         <Container>
           <Row justify="space-between">
-            <Col lg={10} md={10} sm={12} xs={12}>
-              <Language>{t("Contact")}</Language>
-              <Large to="/">{t("Tell us everything")}</Large>
+            <Col lg={12} md={12} sm={24} xs={24}>
+              {/* <Language>{t("Contact")}</Language> */}
+              {/* <Large to="/">{t("Tell us everything")}</Large> */}
               <Para>
                 {t(`Do you have any question? Feel free to reach out.`)}
               </Para>
-              <a href="mailto:l.qqbadze@gmail.com">
+              <a href="mailto:hello@cpflows.com">
                 <Chat>{t(`Let's Chat`)}</Chat>
               </a>
             </Col>
-            <Col lg={8} md={8} sm={12} xs={12}>
-              <Title>{t("Policy")}</Title>
-              <Large to="/" left="true">
-                {t("Application Security")}
-              </Large>
-              <Large left="true" to="/">
-                {t("Software Principles")}
-              </Large>
-            </Col>
-            <Col lg={6} md={6} sm={12} xs={12}>
-              <Empty />
-              <Large left="true" to="/">
-                {t("Support Center")}
-              </Large>
-              <Large left="true" to="/">
-                {t("Customer Support")}
-              </Large>
+            <Col lg={12} md={12} sm={24} xs={24}>
+              <CaptureEmailForm subtext={HeroContent.subtext}/>
             </Col>
           </Row>
+          <br/>
+          <Row justify="center" align="bottom">© 2021 CPFlow. All rights reserved.</Row>
         </Container>
       </FooterSection>
     </>
