c54271fbffa265b644756546170150505beecdf0
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-09-12 19:19:29 -0300

    changed color an underline of hovered links

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -97,8 +97,8 @@ export const Span = styled("span")`
   &:hover,
   &:active,
   &:focus {
-    color: rgb(255, 130, 92);
+    color: #e65665;
     text-underline-position: under;
-    text-decoration: rgb(255, 130, 92) wavy underline;
+    text-decoration: rgb(255, 130, 92) underline;
   }
 `;
