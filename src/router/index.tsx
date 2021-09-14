27c7cc868bfd6dfe0fd4e718b0c35fc9e12b2cc5
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-09-14 20:27:09 -0300

    Added Google Analytics

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,8 +4,18 @@ import Header from "../components/Header";
 import routes from "./config";
 import { Styles } from "../styles/styles";
 import FooterSimple from "../components/FooterSimple";
+import { useEffect } from "react";
+import ReactGA from 'react-ga';
+
+
+ReactGA.initialize('UA-207645967-1');
 
 const Router = () => {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     <Suspense fallback={null}>
       <Styles />
