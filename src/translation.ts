c2f48c5fc8beef2723c6932390134979e3619b9f
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-10-12 15:56:55 -0300

    updated i18next dependencies

--- src/translation.ts
+++ src/translation.ts
@@ -1,13 +1,16 @@
 import i18n from "i18next";
+import { initReactI18next } from 'react-i18next';
+
 import LanguageDetector from "i18next-browser-languagedetector";
-import XHR from "i18next-xhr-backend";
+import Backend from 'i18next-http-backend';
 
 import translationEn from "./locales/en/translation.json";
 import translationEs from "./locales/es/translation.json";
 
 i18n
-  .use(XHR)
+  .use(Backend)
   .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
     debug: false,
     lng: "en",
