530a25d1df8abe028133cac85b84534dafa1436c
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-10-19 12:09:39 -0300

    dynamic year at copyrights text

--- src/sections/FooterSimple/index.tsx
+++ src/sections/FooterSimple/index.tsx
@@ -1,38 +1,35 @@
-import { Row, Col } from "antd";
-import { withTranslation } from "react-i18next";
-import CaptureEmailForm from "../../common/CaptureEmailForm";
-import Container from "../../common/Container";
-import HeroContent from "../../content/HeroContent.json";
+import { Row, Col } from 'antd';
+import { withTranslation } from 'react-i18next';
+import CaptureEmailForm from '../../common/CaptureEmailForm';
+import Container from '../../common/Container';
+import HeroContent from '../../content/HeroContent.json';
 
-import {
-  FooterSection,
-  Para,
-  Chat,
-} from "./styles";
+import { FooterSection, Para, Chat } from './styles';
 
 const FooterSimple = ({ t }: any) => {
-
   return (
     <>
       <FooterSection>
         <Container>
-          <Row justify="space-between">
+          <Row justify='space-between'>
             <Col lg={12} md={12} sm={24} xs={24}>
               {/* <Language>{t("Contact")}</Language> */}
               {/* <Large to="/">{t("Tell us everything")}</Large> */}
               <Para>
                 {t(`Do you have any question? Feel free to reach out.`)}
               </Para>
-              <a href="mailto:hello@cpflows.com">
+              <a href='mailto:hello@cpflows.com'>
                 <Chat>{t(`Let's Chat`)}</Chat>
               </a>
             </Col>
             <Col lg={12} md={12} sm={24} xs={24}>
-              <CaptureEmailForm subtext={HeroContent.subtext}/>
+              <CaptureEmailForm subtext={HeroContent.subtext} />
             </Col>
           </Row>
-          <br/>
-          <Row justify="center" align="bottom">© 2021 CPFlow. All rights reserved.</Row>
+          <br />
+          <Row justify='center' align='bottom'>
+            © {new Date().getFullYear()} CPFlow. All rights reserved.
+          </Row>
         </Container>
       </FooterSection>
     </>
